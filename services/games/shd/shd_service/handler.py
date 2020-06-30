929265d8cc8592db3ce939abd53ca77c6736119e
Lewis Bailey <lebailey@deloitte.com.au>
2020-06-30 22:20:48 +1000

    First attempt at SHD game action route

---
+++ services/games/shd/shd_service/handler.py
@@ -0,0 +1,30 @@
+import json
+import decimal
+from json import JSONEncoder
+from http import HTTPStatus as s
+
+
+class DecimalEncoder(JSONEncoder):
+    def default(self, o): # pylint: disable=method-hidden
+        if isinstance(o, decimal.Decimal):
+            if abs(o) % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+
+def make_response(code: int, body: dict = {}) -> dict:
+    '''Generates HTTP response expected by API gateway'''
+
+    return {
+        'statusCode': code,
+        'headers': {
+                'Access-Control-Allow-Origin': '*'
+        },
+        'body': json.dumps(body, cls=DecimalEncoder)
+    }
+
+def handle(event, context):
+    print(event)
+    return make_response(s.OK, {})
